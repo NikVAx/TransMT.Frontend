27083fb360b20fcf115b097b15d6a1c5e6495601
NikVAx <nik.vasilenko1203@gmail.com>
2024-04-08 13:29:01 +0300

    Add baseButton

---
+++ src/shared/components/baseButton/baseButton.tsx
@@ -0,0 +1,16 @@
+import { Button } from "@adobe/react-spectrum";
+import "./baseButton.style.css";
+import { ComponentPropsWithoutRef, ElementType } from "react";
+
+interface IAs<T> {
+  as?: T;
+}
+
+export const BaseButton = <T extends ElementType = typeof Button>({
+  UNSAFE_className,
+  as, className, ...props
+}: IAs<T> & ComponentPropsWithoutRef<T>) => {
+  const computedClassName = `${UNSAFE_className || className || ""} base-button`;
+  const Component = as || Button;
+  return <Component {...props} UNSAFE_className={computedClassName} />;
+};
